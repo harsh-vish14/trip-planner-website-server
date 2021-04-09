d6c501f40a4b82be47f161152c778f07847cecae
harsh-vish14 <harshkv9322@gmail.com>
2021-04-09 20:49:57 +0530

    added new post request endpoints

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
+import uuid
 
 app = Flask(__name__)
-CORS(app)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+Authkey = '409bb257-c328-42bf-afe8-205f07a4f2be'
 
 cred = credentials.Certificate("./key.json")
 firebase_admin.initialize_app(cred)
@@ -15,50 +18,96 @@ ref = db.collection('users')
 @app.route('/', methods=['GET'])
 def home():
     return jsonify({
-        'message': "hello from flask server"
+        'message': uuid.uuid4()
     })
+
+# @app.route('/hotels', methods=['GET'])
+# def hotel():
+#     return jsonify({
+#         'hotels':'ikasd'
+#     })
+
+@app.route(f'/add-hotel/{Authkey}',methods=['POST'])
+@cross_origin()
+def hotelsAdd():
+    hotelsArray = request.get_json(force=True);
+    # for hotel in hotelsArray:
+
 
 @app.route('/hotels',methods=['GET'])
-def hotel():
-    return jsonify({
-        'hotels':[]
-    })
-
-@app.route('/hotels/<add>',methods=['GET'])
-def hotelsFillter(add = ''):
-    hotels = db.collection('hotel').document('hotels').get().to_dic()
-    filteredHotel = []
-    for hotel in hotels:
-        if hotel.Localtion == add:
-            filteredHotel.append(hotel)
-    return filteredHotel
+def hotelsFillter():
+    hotels = db.collection('hotel').document('hotels').get().to_dict()
+    hotels = hotels['hotelsData']
+    return jsonify(hotels)
 
 
 
 @app.route('/userLogin', methods=['POST'])
+@cross_origin()
 def sendData():
-    userData = request.json
-    print(userData[0])
+    userData = request.get_json(force=True)
     # db.collection('hotel').document('hotels').set(
     #     {
     #         "hotelsData": userData,
     #     }
     # )
     doc_ref = db.collection('users').document(userData['uid'])
+
     doc = doc_ref.get()
-    if doc.exists:
-        doc_ref.set({});
-    else:
+    if doc.exists == False:
         doc_ref.set({
             'name':userData['name'],
             'email':userData['email'],
-            'userPhoto': userData['userPhoto']
-        },);
+            'userPhoto': userData['userPhoto'],
+            'flights': [],
+            'packages': [],
+            'hotels': []
+        });
     return jsonify({
-        'status': 200,
-        'message': 'done'
+        "message": 'User added successfully',
+        "status":200
     })
 
+@app.route('/addFlight', methods=['POST'])
+@cross_origin()
+def add_flight():
+    flightData = request.get_json(force=True)
+    doc_ref = db.collection('users').document(flightData['uid'])
+    # {u'regions': firestore.ArrayUnion([u'greater_virginia'])}
+    doc_ref.update({u'flights': firestore.ArrayUnion([flightData['flightId']])})
+    print(flightData)
+    return jsonify({
+        "message": 'User flight successfully',
+        "status":200
+    })
+
+
+@app.route('/addHotels', methods=['POST'])
+@cross_origin()
+def add_hotels():
+    hotelData = request.get_json(force=True)
+    doc_ref = db.collection('users').document(hotelData['uid'])
+    # {u'regions': firestore.ArrayUnion([u'greater_virginia'])}
+    doc_ref.update({u'hotels': firestore.ArrayUnion([hotelData['hotelId']])})
+    print(hotelData)
+    return jsonify({
+        "message": 'User flight successfully',
+        "status":200
+    })
+
+
+@app.route('/addPackage', methods=['POST'])
+@cross_origin()
+def add_package():
+    packageData = request.get_json(force=True)
+    doc_ref = db.collection('users').document(packageData['uid'])
+    # {u'regions': firestore.ArrayUnion([u'greater_virginia'])}
+    doc_ref.update({u'packages': firestore.ArrayUnion([packageData['packageId']])})
+    print(packageData)
+    return jsonify({
+        "message": 'User flight successfully',
+        "status":200
+    })
 
 @app.route('/flightQuery/<money>/<year>/<month>/<day>', methods=['GET'])
 def fight(money=0, year=0, month=0, day=0):
@@ -77,6 +126,12 @@ def fight(money=0, year=0, month=0, day=0):
 # 'status':404,
 # 'message':'invalid api call'
 # })
+@app.errorhandler(404)
+def not_found(e):
+    return jsonify({
+        "err": 'invalid api call',
+        "status":400,
+    })
 
 if (__name__ == "__main__"):
     app.run(debug=True)
